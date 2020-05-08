d73cdb5ce401288c6781c2c75f2c55095852e907
George Corney <haxiomic@gmail.com>
2020-05-08 23:57:11 +0100

    Add bisect script files

---
+++ Bisect.hx
@@ -0,0 +1,163 @@
+import Util.*;
+import haxe.io.Path;
+import sys.io.File;
+
+using StringTools;
+
+
+/**
+	- Performs commit bisection using precompiled binaries (macOS)
+	- Customize testCommit() and state
+**/
+class Bisect {
+
+	static function main() {
+		var state = try loadState() catch(e: Any) {
+			// Console.error('Setup an initial state in the haxe file (good and bad commits)');
+			// return;
+			// for example:
+			{
+				bad: '1c31a59',
+				good: '6194be1',
+			}
+		}
+
+		Console.examine(state);
+
+		var nightlyHashes = getNightlyUrls()
+		// filter only development branch nightlies
+		.filter(url -> url.contains('development'))
+		.map(url -> {
+			var hashPattern = ~/_([a-z0-9]{7})\.tar\.gz$/i;
+			return if (hashPattern.match(url)) {
+				hashPattern.matched(1).toLowerCase();
+			} else null;
+		})
+		.filter(h -> h != null);
+
+		function iterate() {
+			var goodIndex = nightlyHashes.indexOf(state.good);
+			var badIndex = nightlyHashes.indexOf(state.bad);
+			if (goodIndex == -1) throw 'good hash ${state.good} not found';
+			if (badIndex == -1) throw 'bad hash ${state.bad} not found';
+
+			var midIndex = Math.round((goodIndex + badIndex) * 0.5);
+			var midHash = nightlyHashes[midIndex];
+
+			if (midIndex == goodIndex || midIndex == badIndex) {
+				Console.log('DONE');
+				return;
+			}
+
+			Console.examine(goodIndex, badIndex, midIndex, midHash);
+
+			if (testCommit(midHash)) {
+				Console.log('Hash is <green>good</> <b,green>$midHash</>');
+				state.good = midHash;
+			} else {
+				Console.log('Hash is <red>bad</> <b,red>$midHash</>');
+				state.bad = midHash;
+			}
+
+			saveState(state);
+
+			iterate();
+		}
+
+		iterate();
+	}
+
+	static function testCommit(hash: String) {
+		Console.log('<cyan>Testing commit <b>$hash</b></>');
+		var haxePath = getHaxeExe(hash);
+
+		if (haxePath == null) {
+			throw 'Failed to get prebuilt haxe';
+		}
+
+		cd(Util.haxeDir);
+
+		// throw away local changes
+		exec('git', ['reset', 'HEAD', '--hard']);
+		if (exec('git', ['checkout', hash]) != 0) {
+			throw 'Failed to checkout <b>$hash</>';
+		}
+
+		// patch Printer.hx in the local
+		var printerPath = Path.directory(haxePath) + '/std/haxe/macro/Printer.hx';
+		var printerHx = File.getContent(printerPath);
+
+		// replace 
+		// public function printComplexType(ct:ComplexType) *** public function
+		
+		var startPattern = ~/public function printComplexType\(ct:ComplexType\)/gm;
+		var endPattern = ~/public function/;
+
+		startPattern.match(printerHx);
+		endPattern.match(startPattern.matchedRight());
+		var before = startPattern.matchedLeft();
+		var after = endPattern.matchedRight();
+
+		var newContent = before + replacement + '\tpublic function' +  after;
+
+		Console.log('<yellow>Patching <b>$printerPath</b></>');
+
+		File.saveContent(printerPath, newContent);
+
+		cd('tests/unit');
+
+		// trying compiling js unit tests
+		var haxeResult = process(haxePath, ['compile-js.hxml']);
+
+		if (haxeResult.exit != 0) {
+			Console.examine(haxeResult);
+		}
+
+		return haxeResult.exit == 0;
+	}
+
+	static var replacement = "
+	// @! PATCHED @!
+	public function printComplexType(ct:ComplexType) {
+		return switch (ct) {
+			case TPath(tp): printTypePath(tp);
+			case TFunction(args, ret):
+				var wrapArgumentsInParentheses = switch args {
+					// type `:(a:X) -> Y` has args as [TParent(TNamed(...))], i.e `a:X` gets wrapped in `TParent()`. We don't add parentheses to avoid printing `:((a:X)) -> Y`
+					case [TParent(t)]: false;
+					// this case catches a single argument that's a type-path, so that `X -> Y` prints `X -> Y` not `(X) -> Y`
+					case [TPath(_) | TOptional(TPath(_))]: false;
+					default: true;
+				}
+				var argStr = args.map(printComplexType).join(\", \");
+				(wrapArgumentsInParentheses ? '($argStr)' : argStr) + \" -> \" + (
+
+					/**
+						@! this is the repro of the compiler issue:
+
+						- The following expression should be equivalent to just `printComplexType(ret)` because the switch is redundant
+						- When testing in simple examples everything works as expected
+						- When compiling the unit tests (e.g. `haxe compile-js.hxml`), haxe fails with:
+							`Cannot use Void as value`
+						- If it's tweaked to compile it generates the wrong code on all targets
+						- It doesn't seem to matter what we're switching on, so I've just used `true` for simplicity (see #9385 for original example)
+					**/
+					switch true {
+						default: printComplexType(ret);
+					}
+
+				);
+			case TAnonymous(fields): \"{ \" + [for (f in fields) printField(f) + \"; \"].join(\"\") + \"}\";
+			case TParent(ct): \"(\" + printComplexType(ct) + \")\";
+			case TOptional(ct): \"?\" + printComplexType(ct);
+			case TNamed(n, ct): n + \":\" + printComplexType(ct);
+			case TExtend(tpl, fields): '{> ${tpl.map(printTypePath).join(\" >, \")}, ${fields.map(printField).join(\", \")} }';
+			case TIntersection(tl): tl.map(printComplexType).join(\" & \");
+		}
+	}
+
+	// @! PATCHED @!
+
+";
+
+}
---
+++ Util.hx
@@ -0,0 +1,143 @@
+import haxe.io.Path;
+import sys.FileSystem;
+import sys.io.File;
+import sys.io.Process;
+
+using Lambda;
+using StringTools;
+
+typedef State = {
+	var bad: String;
+	var good: String;
+}
+
+class Util {
+
+	static public final stateFilePath = 'bisect-state.json';
+	static public final initialCwd = sys.FileSystem.absolutePath(Sys.getCwd());
+	static public final downloadDir = Path.join([initialCwd, 'downloads']);
+	static public final haxeDir = Path.join([initialCwd, '../']);
+
+	static var pageCached = null;
+	static public function getNightlyUrls() {
+		var page = if (pageCached == null) {
+			pageCached = runInDir(downloadDir, () -> {
+				try File.getContent('index.html') catch (e: Any) {
+					exec('wget', ['https://build.haxe.org/builds/haxe/mac/']);
+					// older here: https://hxbuilds.s3.us-east-1.amazonaws.com/builds/haxe/mac/index.html
+					File.getContent('index.html');
+				}
+			});
+		} else pageCached;
+		if (page == null) {
+			Console.error('Failed to get page');
+			return null;
+		}
+		var r = ~/href="([^"]+)"/igm;
+		var urls = new Array<String>();
+		r.map(page, (r) -> {urls.push(r.matched(1).toLowerCase()); '$';});
+		return urls;
+	}
+
+	static public function getNightlyFilename(hash: String) {
+		// find matching url
+		var shortHash = hash.substr(0, 7).toLowerCase();
+		return getNightlyUrls().find(u -> {
+			u.contains(shortHash);
+		});
+	}
+
+	static public function getHaxeExe(hash: String) {
+		return runInDir(downloadDir, () -> {
+			// download haxe build for this hash
+			var nightlyFilename = getNightlyFilename(hash);
+
+			if (nightlyFilename == null) {
+				Console.error('Failed to find download for <b>$hash</>');
+				return null;
+			}
+
+			if (!FileSystem.exists(nightlyFilename)) {
+				Console.log('Downloading <b>$nightlyFilename</b>');
+				var url = 'https://build.haxe.org/builds/haxe/mac/$nightlyFilename';
+				exec('wget', [url]);
+			}
+
+			// unpack
+			var unzipDir = Path.withoutExtension(Path.withoutExtension(nightlyFilename));
+			exec('mkdir', ['-p', unzipDir]);
+			exec('tar', ['-xzf', nightlyFilename, '-C', unzipDir]);
+
+			// get haxe exe
+			// find haxe_2019-09-12_4.0_bugfix_4a74534 -name "haxe" -type f -maxdepth 2
+			var p = new Process('find', [unzipDir, '-name', 'haxe', '-type', 'f', '-maxdepth', '2']);
+			var haxePath = if (p.exitCode(true) == 0) {
+				FileSystem.absolutePath(p.stdout.readAll().toString().trim());
+			} else throw 'find failed';
+
+			return haxePath;
+		});
+	}
+
+	static public function runInDir<T>(dir: String, fn: () -> T): T {
+		var originalCwd = Path.normalize(Sys.getCwd());
+		if (originalCwd != Path.normalize(dir)) {
+			cd(dir);
+		}
+		var ret = fn();
+		if (Path.normalize(Sys.getCwd()) != originalCwd) {
+			cd(originalCwd);
+		}
+		return ret;
+	}
+
+	static public function exec(cmd: String, ?args: Array<String>) {
+		if (args == null) args = [];
+		Console.log('Exec: <b>$cmd ${args.join(' ')}</>');
+		var c = Sys.command(cmd, args);
+		if (c != 0) {
+			Console.log('<b,red>\t -> exit: $c</>');
+		}
+		return c;
+	}
+
+	static public function process(cmd: String, ?args: Array<String>) {
+		if (args == null) args = [];
+
+		Console.log('Process: <b>$cmd ${args.join(' ')}</>');
+
+		var p = new Process(cmd, args);
+		var c = p.exitCode(true);
+
+		if (c != 0) {
+
+			Console.log('<b,red>\t -> exit: $c</>');
+		}
+
+		return {
+			stdout: p.stdout.readAll().toString(),
+			stderr: p.stderr.readAll().toString(),
+			exit: c,
+		}
+	}
+
+	static public function cd(dir: String) {
+		Console.log('cd(<b>$dir</>)');
+		Sys.setCwd(dir);
+	}
+
+	static public function loadState(): State {
+		return runInDir(initialCwd, () -> {
+			Console.log('Trying to load state from <b>${Path.join([Sys.getCwd(), stateFilePath])}</>');
+			haxe.Json.parse(File.getContent(stateFilePath));
+		});
+	}
+
+	static public function saveState(state: State) {
+		runInDir(initialCwd, () -> {
+			Console.log('Saving state to <b>${Path.join([Sys.getCwd(), stateFilePath])}</>');
+			File.saveContent(stateFilePath, haxe.Json.stringify(state, null, '\t'));
+		});
+	}
+
+}
